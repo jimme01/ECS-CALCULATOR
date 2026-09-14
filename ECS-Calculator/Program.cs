bfadf35014ac7a4869bbcb4e3b2a8f6334b2acdb
jimme01 <kosmach06@gmail.com>
2026-09-14 23:05:16 +0300

    Update version string v1.1

--- ECS-Calculator/Program.cs
+++ ECS-Calculator/Program.cs
@@ -10,7 +10,7 @@ namespace ECS_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Консольний калькулятор v1.0");
+            Console.WriteLine("Консольний калькулятор v1.1 (степінь та остача)");
         }
     }
 }
