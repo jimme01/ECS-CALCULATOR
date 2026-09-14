269d6a37ecbb7fe71c47afe51d69aa7fa2bf9784
ArtemMostip <artemmostipaka07@gmail.com>
2026-09-14 22:32:26 +0300

    Add addition and subtraction operations

---
+++ ECS-Calculator/Operations/AddSubtract.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ECS_Calculator.Operations
+{
+    public static class AddSubtract
+    {
+        public static double Add(double a, double b) => a + b;
+        public static double Subtract(double a, double b) => a - b;
+    }
+}
