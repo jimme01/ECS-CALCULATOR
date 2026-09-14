35d619195bbcdab22b9bbece1f6af41ecb41ca08
jimme01 <kosmach06@gmail.com>
2026-09-14 22:21:09 +0300

    Add power and modulo operations

---
+++ ECS-Calculator/Operations/PowerModulo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ECS_Calculator.Operations
+{
+    public static class PowerModulo
+    {
+        public static double Power(double baseNumber, double exponent) => Math.Pow(baseNumber, exponent);
+        public static double Modulo(double a, double b) => a % b;
+    }
+}
